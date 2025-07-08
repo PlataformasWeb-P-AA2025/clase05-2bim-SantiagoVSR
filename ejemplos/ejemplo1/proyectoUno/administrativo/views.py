b690db5772f74f911f24f8eedfa92ecaccbb8246
SantiagoVSR <santiago9sr@hotmail.com>
2025-07-08 09:04:11 -0500

    actiacion01

--- ejemplos/ejemplo1/proyectoUno/administrativo/views.py
+++ ejemplos/ejemplo1/proyectoUno/administrativo/views.py
@@ -91,7 +91,8 @@ def crear_estudiante(request):
     return render(request, 'crearEstudiante.html', diccionario)
 
 
-@login_required(login_url='/entrando/login/')
+# @login_required(login_url='/entrando/login/')
+@login_required
 @permission_required('administrativo.change_estudiante',)
 def editar_estudiante(request, id):
     """
@@ -109,7 +110,8 @@ def editar_estudiante(request, id):
 
     return render(request, 'editarEstudiante.html', diccionario)
 
-
+@login_required
+@permission_required('administrativo.delete_estudiante', )
 def eliminar_estudiante(request, id):
     """
     """
@@ -125,6 +127,9 @@ def en_grupo(nombre):
     return user_passes_test(predicate)
 
 @en_grupo('supervisor')
+
+@login_required
+@permission_required('administrativo.add_numerotelefonico',)
 def crear_numero_telefonico(request):
     """
     """
@@ -141,7 +146,8 @@ def crear_numero_telefonico(request):
 
     return render(request, 'crearNumeroTelefonico.html', diccionario)
 
-
+@login_required
+@permission_required('administrativo.change_numerotelefonico',)
 def editar_numero_telefonico(request, id):
     """
     """
@@ -158,6 +164,8 @@ def editar_numero_telefonico(request, id):
 
     return render(request, 'crearNumeroTelefonico.html', diccionario)
 
+@login_required
+@permission_required('administrativo.change_estudiante', )
 def crear_numero_telefonico_estudiante(request, id):
     """
     """
